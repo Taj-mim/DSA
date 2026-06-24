a5a52d625f6f974e3bd8357114c04dc4944d6c81
Taj-mim <tajmim915@gmail.com>
2026-06-25 03:19:31 +0600

    describe why using loop is the best approch for fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -3,7 +3,8 @@ n2=1
 n=int(input())
 print(n1)
 print(n2)
-for i in range(n-2): #using loops 
+for i in range(n-2): #using loops is the best approach for 
+                        #fibonacci as it takes O(n) time complexity and O(1) space complexity 
     fib=n1+n2
     print(fib)
     n1=n2
