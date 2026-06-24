ec83092c01bc9c71f9335c00b87dd2f1d0fd6dfa
Taj-mim <tajmim915@gmail.com>
2026-06-25 03:43:38 +0600

    add time and space complexity details

--- fibonacci-recursion.py
+++ fibonacci-recursion.py
@@ -1,6 +1,6 @@
 #for nth term value of fibonacci series using recursion
 def fibo(n):
-    if n<=1:
+    if n<=1:       #time complexity O(2^n) and space complexity is O(n)
         return n
     else:
         return fibo(n-1)+fibo(n-2)
